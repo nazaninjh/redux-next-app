9db19b63e9219d438e7ac2002ece1ed00fb7f9bf
nazaninjh <nazanin.jh97@gmail.com>
2024-06-27 14:45:03 +0330

    Add options btn for each blog and styled it

--- app/blog/page.jsx
+++ app/blog/page.jsx
@@ -8,16 +8,22 @@ import style from './../page.module.css';
 import { selectAllUsers } from "../../lib/features/users/usersSlice";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEllipsisV } from "@fortawesome/free-solid-svg-icons";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { current } from "@reduxjs/toolkit";
 export default function Blogpage() {
   // add edit
   // get the first 5 blogs and add pagination
+  const optionsRef = useRef({current: 1});
   const {
     isSuccess,
     isLoading,
     isError,
     error
   } = useGetAllBlogsQuery();
+  const toggleElipsis = (e) => {
+    optionsRef.current = e.target.id;
+    setElipsisClicked(!elipsisClicked);
+  }
   const [ elipsisClicked, setElipsisClicked ] = useState(false)
   const blogs = useSelector(state => selectAllBlogs(state));
   const users = useSelector(state => selectAllUsers(state));
@@ -25,19 +31,30 @@ export default function Blogpage() {
   if (isSuccess) {
     // eslint-disable-next-line react-hooks/exhaustive-deps
     content = blogs.map(blog => {
+        // console.log('id', blog.id);
+        // if (optionsRef.current) {
+        //   console.log('class', optionsRef.current)
+        // }
+        
         const user = users.find(user => user.id === blog.userId);
         return (
           <article key={blog.id}
           className={style.blog}>
             <span>
-              <button onClick={() => setElipsisClicked(!elipsisClicked)}>
-                <FontAwesomeIcon icon={faEllipsisV} inverse/>
+              <button 
+              id={blog.id}
+              onClick={(e) => toggleElipsis(e)}>
+                <FontAwesomeIcon icon={faEllipsisV}
+                style={{fontSize: 'large'}}
+                inverse
+                />
               </button>
             </span>
-            {elipsisClicked && <div>
+            {( elipsisClicked && optionsRef.current === blog.id ) && 
+            <div className={style.optionsIconNav}>
               <Link href='/blog/edit'>Edit</Link>
               <Link href='/blog/delete'>Delete</Link>
-              </div>}
+            </div>}
             
             <h2>Posted by <Link href={`/users/${user.id}`}>{user.name}</Link></h2>
             <h4>{blog.title}</h4>
